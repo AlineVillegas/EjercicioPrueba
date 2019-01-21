a21002dec8f30f2504f9c4dfd994bdf826cc8c87
AlineVillegas <46898124+AlineVillegas@users.noreply.github.com>
2019-01-21 09:41:21 -0600

    Aline Villegas Berdejo. Grupo: 02

---
+++ EjercicioVelocidadPromedio.py
@@ -0,0 +1,10 @@
+#Autor: Aline Villegas Berdejo
+#Calcula la velocidad promedio de un viaje
+
+tiempo=int(input("Teclea el tiempo del viaje en horas: "))
+distancia=int(input("Teclea la distancia del viaje en kilometros: "))
+
+velocidad=tiempo / distancia
+
+print("La Velocidad Promedio es: ", velocidad, "km/h")
+
